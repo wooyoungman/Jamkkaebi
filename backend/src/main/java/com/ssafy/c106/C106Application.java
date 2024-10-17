5d89a2ccb3e4c687c803cd6ddc60846e73c34961
송준혁 <fprhqkrtk303@naver.com>
2024-10-17 10:20:18 +0900

    init: Spring Boot 프로젝트 추가

---
+++ backend/src/main/java/com/ssafy/c106/C106Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.c106;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C106Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/c106/C106ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.c106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
