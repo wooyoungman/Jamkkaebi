5e5db90a316789a262e433c6b22601670ee54700
송준혁 <fprhqkrtk303@naver.com>
2024-10-24 10:15:07 +0900

    feat: 예외 핸들러에 RuntimeException 예외 등록

--- backend/src/main/java/com/ssafy/c106/common/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/c106/common/handler/GlobalExceptionHandler.java
@@ -1,7 +1,17 @@
 package com.ssafy.c106.common.handler;
 
+import com.ssafy.c106.common.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<Void>> handleRuntimeException(RuntimeException e) {
+        ApiResponse<Void> response = ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
 }
