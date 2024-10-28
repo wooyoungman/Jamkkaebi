05691078f35bed7ebf3841bbafadaed68bc8d2a1
송준혁 <fprhqkrtk303@naver.com>
2024-10-28 20:47:55 +0900

    refactor: 유저 정보 조회 서비스 단순화
    - Member 엔티티는 이미 UserDetails를 확장하므로, 별도의 레포지토리 조회 및 생성
      로직이 필요 없음

--- backend/src/main/java/com/ssafy/c106/common/security/jwt/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/ssafy/c106/common/security/jwt/service/CustomUserDetailsService.java
@@ -1,10 +1,8 @@
 package com.ssafy.c106.common.security.jwt.service;
 
-import com.ssafy.c106.domain.member.entity.Member;
 import com.ssafy.c106.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,15 +18,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return memberRepository.findByUsername(username)
-                .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found: " + username));
-    }
-
-    private UserDetails createUserDetails(Member member) {
-        return User.builder()
-                .username(member.getUsername())
-                .password(member.getPassword())
-                .authorities(member.getAuthorities())
-                .build();
     }
 }
--- backend/src/main/java/com/ssafy/c106/common/security/util/SecurityUtil.java
+++ backend/src/main/java/com/ssafy/c106/common/security/util/SecurityUtil.java
@@ -2,17 +2,13 @@ package com.ssafy.c106.common.security.util;
 
 import com.ssafy.c106.domain.member.entity.Member;
 import com.ssafy.c106.domain.member.exception.UserNotFoundException;
-import com.ssafy.c106.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @RequiredArgsConstructor
 public class SecurityUtil {
-
-    private final MemberRepository memberRepository;
 
     public Long getCurrentUserId() {
         return getAuthentication().getId();
@@ -25,9 +21,7 @@ public class SecurityUtil {
     private Member getAuthentication() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null && auth.getPrincipal() instanceof UserDetails) {
-            String username = ((UserDetails) auth.getPrincipal()).getUsername();
-            return memberRepository.findByUsername(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User Not Found: " + username));
+            return (Member) auth.getPrincipal();
         }
         throw new UserNotFoundException();
     }
