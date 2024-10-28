a57ce498055a2580f950149cff2abe346d938527
이정준 <ofekim0@naver.com>
2024-10-28 16:49:10 +0900

    feat:기본 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import router from "./routes.tsx";
+import { RouterProvider } from "react-router-dom";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
+
+const queryClient = new QueryClient();
+
+// if (typeof global === "undefined") {
+//   (window as any).global = window;
+// }
+
+createRoot(document.getElementById("root")!).render(
+  // <StrictMode>
+  <ErrorBoundary
+    FallbackComponent={ErrorFallback}
+    onReset={() => {
+      console.log("ErrorBoundary reset, retrying...");
+    }}
+  >
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </ErrorBoundary>
+  // </StrictMode>,
+);
