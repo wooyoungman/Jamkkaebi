508f64b39da8c75c5b4e9846d95a6864adb46d8f
송준혁 <fprhqkrtk303@naver.com>
2024-10-30 11:59:45 +0900

    feat: 현재 유저의 권한 추출 메서드
    인증 정보 추출 시 Member 인스턴스인지 확인하도록 변경

--- backend/src/main/java/ssafy/modo/jamkkaebi/common/security/util/SecurityUtil.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/common/security/util/SecurityUtil.java
@@ -1,12 +1,16 @@
 package ssafy.modo.jamkkaebi.common.security.util;
 
-import ssafy.modo.jamkkaebi.domain.member.entity.Member;
-import ssafy.modo.jamkkaebi.domain.member.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import ssafy.modo.jamkkaebi.domain.member.entity.Member;
+import ssafy.modo.jamkkaebi.domain.member.entity.MemberRole;
+import ssafy.modo.jamkkaebi.domain.member.exception.UserNotFoundException;
 
+@Slf4j
+@Component
 @RequiredArgsConstructor
 public class SecurityUtil {
 
@@ -18,11 +22,17 @@ public class SecurityUtil {
         return getAuthentication().getUsername();
     }
 
+    public MemberRole getCurrentUserRole() {
+        return getAuthentication().getRole();
+    }
+
     private Member getAuthentication() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null && auth.getPrincipal() instanceof UserDetails) {
+        if (auth != null && auth.getPrincipal() instanceof Member) {
             return (Member) auth.getPrincipal();
+        } else {
+            log.info("Principal instance: {}", auth.getPrincipal().getClass());
+            throw new UserNotFoundException();
         }
-        throw new UserNotFoundException();
     }
 }
