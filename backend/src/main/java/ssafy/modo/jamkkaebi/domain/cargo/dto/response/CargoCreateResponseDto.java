6938ff993737f80bbdc179ab9a26aa6013248fb0
송준혁 <fprhqkrtk303@naver.com>
2024-11-06 17:23:16 +0900

    refactor: 화물 생성 완료 응답 DTO 수정

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/cargo/dto/response/CargoCreateResponseDto.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/cargo/dto/response/CargoCreateResponseDto.java
@@ -2,17 +2,15 @@ package ssafy.modo.jamkkaebi.domain.cargo.dto.response;
 
 import lombok.Builder;
 import lombok.Data;
-import ssafy.modo.jamkkaebi.domain.route.dto.GeoJsonDto;
-
-import java.util.Map;
 
 @Data
 @Builder
 public class CargoCreateResponseDto {
 
+    private long cargoId;
+    private String routeId;
+    private String cargo;
     private String origin;
     private String destination;
-    private Map<String, Float> originCoordinate;
-    private Map<String, Float> destinationCoordinate;
-    private GeoJsonDto route;
+    private int distance;
 }
