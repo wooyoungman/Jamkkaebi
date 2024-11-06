72424134157a4ea197c8f489cff55b73e6eabb5e
송준혁 <fprhqkrtk303@naver.com>
2024-11-06 17:22:50 +0900

    refactor: 화물 엔티티 수정
    - 좌표 필드 분리
    - Builder 사용한 생성자 추가
    - 경로 업데이트 메서드 추가

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/cargo/entity/Cargo.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/cargo/entity/Cargo.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ssafy.modo.jamkkaebi.common.entity.BaseEntity;
@@ -28,17 +29,43 @@ public class Cargo extends BaseEntity {
     private String origin;
 
     @NotNull
-    private String originCoordinate;
+    private String originLat;
+
+    @NotNull
+    private String originLon;
 
     @NotNull
     private String destination;
 
     @NotNull
-    private String destinationCoordinate;
+    private String destinationLat;
+
+    @NotNull
+    private String destinationLon;
 
     @NotNull
     private LocalDateTime dueDate;
 
     @NotNull
-    private Float distance;
+    private Integer distance;
+
+    private String routeId;
+
+    @Builder
+    public Cargo(String cargo, String origin, String destination, LocalDateTime dueDate, Integer distance,
+                 String originLat, String originLon, String destinationLat, String destinationLon) {
+        this.cargo = cargo;
+        this.origin = origin;
+        this.originLat = originLat;
+        this.originLon = originLon;
+        this.destination = destination;
+        this.destinationLat = destinationLat;
+        this.destinationLon = destinationLon;
+        this.dueDate = dueDate;
+        this.distance = distance;
+    }
+
+    public void updateRouteId(String routeId) {
+        this.routeId = routeId;
+    }
 }
