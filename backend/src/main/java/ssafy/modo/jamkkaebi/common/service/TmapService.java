8832204352167d442065813dae355cc9d63b6f39
송준혁 <fprhqkrtk303@naver.com>
2024-11-06 10:21:04 +0900

    feat: TMAP API 서비스 추가
    - 지오코딩 API

---
+++ backend/src/main/java/ssafy/modo/jamkkaebi/common/service/TmapService.java
@@ -0,0 +1,80 @@
+package ssafy.modo.jamkkaebi.common.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.GsonBuilder;
+import com.nimbusds.jose.shaded.gson.JsonObject;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+import ssafy.modo.jamkkaebi.common.dto.AddressQueryDto;
+import ssafy.modo.jamkkaebi.common.util.RequestUtil;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TmapService {
+
+    private final RequestUtil requestUtil;
+
+    @Value("${tmap.base-url}")
+    private String baseUrl;
+
+    @Value("${tmap.app-key}")
+    private String appKey;
+
+    private static final Integer version = 1;
+
+    private Map<String, String> buildHeader() {
+        Map<String, String> header = new HashMap<>();
+        header.put("appKey", appKey);
+        return header;
+    }
+
+    private String buildUrlWithParams(String url, String address) {
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
+        AddressQueryDto queryDto = new AddressQueryDto(address, version, appKey);
+
+        for (Field field : AddressQueryDto.class.getDeclaredFields()) {
+            field.setAccessible(true);
+            try {
+                Object value = field.get(queryDto);
+                if (value != null) {
+                    builder.queryParam(field.getName(), value);
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("Error accessing field: " + field.getName(), e);
+            }
+        }
+
+        return builder.toUriString();
+    }
+
+    public Map<String, Float> getGeoCoordinate(String address) throws JsonProcessingException {
+
+        Map<String, Float> geoCoordinate = new HashMap<>();
+
+        String requestUrl = buildUrlWithParams(baseUrl + "/geo/fullAddrGeo", address);
+        ResponseEntity<String> response = requestUtil.sendRequest(HttpMethod.GET, requestUrl, String.class, null);
+
+        Gson gson = new GsonBuilder().create();
+        JsonObject jsonBody = gson.fromJson(response.getBody(), JsonObject.class)
+                .getAsJsonObject("coordinateInfo")
+                .getAsJsonArray("coordinate")
+                .get(0).getAsJsonObject();
+
+        geoCoordinate.put("lat", jsonBody.getAsJsonPrimitive("newLat").getAsFloat());
+        geoCoordinate.put("lon", jsonBody.getAsJsonPrimitive("newLon").getAsFloat());
+
+        return geoCoordinate;
+    }
+}
