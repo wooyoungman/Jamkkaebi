07f5a754bd27a548d834af6aa0838957e53d11eb
송준혁 <fprhqkrtk303@naver.com>
2024-11-07 13:45:08 +0900

    feat: 매니저의 운전자 조회 서비스 생성

---
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/manager/service/ManagerReadService.java
@@ -0,0 +1,48 @@
+package ssafy.modo.jamkkaebi.domain.manager.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ssafy.modo.jamkkaebi.domain.manager.dto.response.DriversResponseDto;
+import ssafy.modo.jamkkaebi.domain.manager.dto.response.SimpleDriverInfo;
+import ssafy.modo.jamkkaebi.domain.member.entity.Member;
+import ssafy.modo.jamkkaebi.domain.member.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ManagerReadService {
+
+    private final MemberRepository memberRepository;
+
+    private static SimpleDriverInfo setDriverInfo(Member driver) {
+        return SimpleDriverInfo.builder()
+                .driverId(driver.getId())
+                .driverName(driver.getName())
+                .address(null)
+                .phoneNumber(null)
+                .build();
+    }
+
+    public DriversResponseDto getManagedDrivers() {
+
+        List<SimpleDriverInfo> drivers = memberRepository.findManagedDriver()
+                .stream().map(ManagerReadService::setDriverInfo).toList();
+
+        return DriversResponseDto.builder()
+                .count(drivers.size())
+                .drivers(drivers)
+                .build();
+    }
+
+    public DriversResponseDto getUnmanagedDrivers() {
+
+        List<SimpleDriverInfo> drivers = memberRepository.findUnmanagedDriver()
+                .stream().map(ManagerReadService::setDriverInfo).toList();
+
+        return DriversResponseDto.builder()
+                .count(drivers.size())
+                .drivers(drivers)
+                .build();
+    }
+}
