596661593bcb623f0c1bdfaa75372280f929bcd8
송준혁 <fprhqkrtk303@naver.com>
2024-11-07 13:41:46 +0900

    feat: 관리자 정보 기반의 운전자 조회 쿼리

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/member/repository/MemberRepository.java
@@ -1,11 +1,27 @@
 package ssafy.modo.jamkkaebi.domain.member.repository;
 
-import ssafy.modo.jamkkaebi.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ssafy.modo.jamkkaebi.domain.member.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUsername(String username);
+
+    @Query("""
+            SELECT m FROM Member m
+            LEFT JOIN ManagerAndDriver md ON m.id = md.driver.id
+            WHERE m.role = 'DRIVER' AND md.driver.id IS NULL
+            """)
+    List<Member> findUnmanagedDriver();
+
+    @Query("""
+            SELECT m FROM Member m
+            LEFT JOIN ManagerAndDriver md ON m.id = md.driver.id
+            WHERE m.role = 'DRIVER' AND md.driver.id IS NOT NULL
+            """)
+    List<Member> findManagedDriver();
 }
