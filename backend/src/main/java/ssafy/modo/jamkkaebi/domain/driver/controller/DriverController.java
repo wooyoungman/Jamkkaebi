d33b283f1015f91dea81bc387cd6ceef55ac3990
송준혁 <fprhqkrtk303@naver.com>
2024-11-08 17:18:44 +0900

    feat: 운전자 컨트롤러 추가

---
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/driver/controller/DriverController.java
@@ -0,0 +1,11 @@
+package ssafy.modo.jamkkaebi.domain.driver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/driver")
+@RequiredArgsConstructor
+public class DriverController {
+}
