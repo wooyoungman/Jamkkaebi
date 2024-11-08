375dd2ac16ee064aae5e98ec4c8bdfbfd428c876
송준혁 <fprhqkrtk303@naver.com>
2024-11-08 17:06:32 +0900

    chore: 번호판 regex 수정

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/validation/VehiclePlateValidator.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/validation/VehiclePlateValidator.java
@@ -5,7 +5,7 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class VehiclePlateValidator implements ConstraintValidator<ValidVehiclePlate, String> {
 
-    private static final String KOREAN_PLATE_REGEX = "^[0-9]{2,3}[가-힣] [0-9]{4}$";
+    private static final String KOREAN_PLATE_REGEX = "\\d{2,3}[가-힣] \\d{4}$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
