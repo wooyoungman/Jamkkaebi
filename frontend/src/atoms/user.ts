0605f00d5f86dcc3f7b43ea5d365b355342e2401
이예지 <lllyyj75@gmail.com>
2024-11-08 16:59:50 +0900

    chore : localStorage 수정

--- frontend/src/atoms/user.ts
+++ frontend/src/atoms/user.ts
@@ -8,8 +8,8 @@ interface AuthState {
 
 // auth 상태를 관리하는 atom
 export const authAtom = atom<AuthState>({
-  isAuthenticated: !!sessionStorage.getItem("accessToken"),
-  accessToken: sessionStorage.getItem("accessToken"),
+  isAuthenticated: !!localStorage.getItem("accessToken"),
+  accessToken: localStorage.getItem("accessToken"),
 });
 
 // login mutation용 atom
@@ -17,7 +17,7 @@ export const loginAtom = atom(
   null, // read - 사용하지 않으므로 null
   (get, set, accessToken: string) => {
     // sessionStorage에 저장
-    sessionStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("accessToken", accessToken);
     // atom 상태 업데이트
     set(authAtom, {
       isAuthenticated: true,
@@ -29,7 +29,7 @@ export const loginAtom = atom(
 // logout용 atom
 export const logoutAtom = atom(null, (get, set) => {
   // sessionStorage에서 제거
-  sessionStorage.removeItem("accessToken");
+  localStorage.removeItem("accessToken");
   // atom 상태 초기화
   set(authAtom, {
     isAuthenticated: false,
