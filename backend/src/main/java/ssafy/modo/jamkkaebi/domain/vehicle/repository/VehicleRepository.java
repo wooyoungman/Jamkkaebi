0d0a617871511bb9450ba477aa22d2cde54e2981
송준혁 <fprhqkrtk303@naver.com>
2024-11-08 22:41:29 +0900

    feat: 운전자 ID로 차량 조회 쿼리 추가

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/repository/VehicleRepository.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/repository/VehicleRepository.java
@@ -1,9 +1,18 @@
 package ssafy.modo.jamkkaebi.domain.vehicle.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ssafy.modo.jamkkaebi.domain.vehicle.entity.Vehicle;
+
+import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     Boolean existsByVehicleNumber(String vehicleNumber);
+
+    @Query("""
+            SELECT v FROM Vehicle v
+            WHERE v.driver.id = :driverId
+            """)
+    Optional<Vehicle> findByDriverId(Long driverId);
 }
