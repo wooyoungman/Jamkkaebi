864b1f16e47dcdb6586b62a86742521ab0c980ef
송준혁 <fprhqkrtk303@naver.com>
2024-11-08 11:46:06 +0900

    chore: 전역 오류 핸들링 페이지 메서드 명시 삭제

--- backend/src/main/java/ssafy/modo/jamkkaebi/common/controller/ErrorControllerImpl.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/common/controller/ErrorControllerImpl.java
@@ -3,15 +3,12 @@ package ssafy.modo.jamkkaebi.common.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ErrorControllerImpl implements ErrorController {
 
-    @RequestMapping(value = "/error", method = {
-            RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE
-    })
+    @RequestMapping(path = "/error")
     public void handleError(HttpServletRequest request) throws Throwable {
         if (request.getAttribute("jakarta.servlet.error.exception") != null) {
             throw (Throwable) request.getAttribute("jakarta.servlet.error.exception");
