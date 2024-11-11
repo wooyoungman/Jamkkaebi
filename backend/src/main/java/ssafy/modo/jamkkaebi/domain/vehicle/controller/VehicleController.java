ff901608b4cfd6db99d18726c8ce93265b33c6cf
송준혁 <fprhqkrtk303@naver.com>
2024-11-11 23:45:39 +0900

    feat: 차량 조작을 위한 API 엔드포인트

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/controller/VehicleController.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/controller/VehicleController.java
@@ -3,12 +3,11 @@ package ssafy.modo.jamkkaebi.domain.vehicle.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ssafy.modo.jamkkaebi.common.ApiResponse;
+import ssafy.modo.jamkkaebi.domain.vehicle.dto.request.VehicleControlRequestDto;
 import ssafy.modo.jamkkaebi.domain.vehicle.dto.request.VehicleCreateRequestDto;
+import ssafy.modo.jamkkaebi.domain.vehicle.dto.response.VehicleControlResponseDto;
 import ssafy.modo.jamkkaebi.domain.vehicle.dto.response.VehicleCreateResponseDto;
 import ssafy.modo.jamkkaebi.domain.vehicle.service.VehicleWriteService;
 
@@ -23,5 +22,12 @@ public class VehicleController {
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ApiResponse<VehicleCreateResponseDto> registerVehicle(@Valid @RequestBody VehicleCreateRequestDto dto) {
         return ApiResponse.success(vehicleWriteService.registerVehicle(dto));
+    }
+
+    @PatchMapping(path = "/control/command/{vehicleId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<VehicleControlResponseDto> controlCommand(
+            @PathVariable Long vehicleId, @Valid @RequestBody VehicleControlRequestDto dto) {
+        return ApiResponse.success(vehicleWriteService.controlByCommand(vehicleId, dto));
     }
 }
