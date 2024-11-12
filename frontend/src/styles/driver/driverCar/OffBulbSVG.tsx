89c09f84ce299ee365597eb1e06537e79b3781e9
이정준 <ofekim0@naver.com>
2024-11-12 21:22:45 +0900

    .

--- frontend/src/styles/driver/driverCar/OffBulbSVG.tsx
+++ frontend/src/styles/driver/driverCar/OffBulbSVG.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 // import OffBulbIcon from "@styles/driver/driverCar/OffBulbRenewal.svg?react";
 // import OffBulbIconV2 from "@styles/driver/driverCar/OffBulbV2.svg?react";
-import OnBulbIconV1 from "@styles/driver/driverCar/OnBulbV1.svg?react";
-import OffBulbIcon from "@styles/driver/driverCar/OffBulbV3.svg?react";
-import bulbTailImg from "@assets/bulbTail.png";
+import OnBulbIconV1 from "@styles/driver/driverCar/OnBulb.svg?react";
+// import OffBulbIcon from "@styles/driver/driverCar/OffBulbV3.svg?react";
+// import bulbTailImg from "@assets/bulbTail.png";
 import styled from "styled-components";
 
 const BulbContainer = styled.div`
@@ -12,19 +12,20 @@ const BulbContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
-// 스타일 컴포넌트 정의
-const StyledBulbTail = styled.img`
-  width: 75px;
-  height: 80px;
-`;
+
+// // 스타일 컴포넌트 정의
+// const StyledBulbTail = styled.img`
+//   width: 75px;
+//   height: 80px;
+// `;
 
 const OffBulbSVG: React.FC = () => {
   // return <OffBulbIconV2 />;
   return (
     <BulbContainer>
-      <StyledBulbTail src={bulbTailImg} />
-      <OffBulbIcon />
-      {/* <OnBulbIconV1 /> */}
+      {/* <StyledBulbTail src={bulbTailImg} /> */}
+      {/* <OffBulbIcon /> */}
+      <OnBulbIconV1 />
     </BulbContainer>
   );
 };
