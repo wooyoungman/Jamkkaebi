50e38a2ea2e487c56352c603b3d519b0103c8cec
송준혁 <fprhqkrtk303@naver.com>
2024-11-13 15:42:49 +0900

    fix: RabbitMQ 데이터 수신 Topic 명시

--- backend/src/main/java/ssafy/modo/jamkkaebi/common/rabbitmq/config/RabbitConfig.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/common/rabbitmq/config/RabbitConfig.java
@@ -1,6 +1,9 @@
 package ssafy.modo.jamkkaebi.common.rabbitmq.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,6 +29,6 @@ public class RabbitConfig {
     @Bean
     public Binding binding(AnonymousQueue autoDeleteQueue) {
         return BindingBuilder.bind(autoDeleteQueue).to(new TopicExchange("amq.topic"))
-                .with("device.#");
+                .with("device.#.data");
     }
 }
