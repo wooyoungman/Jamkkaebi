864748c7e501ffae496cf93516fbb71b54e2d4d7
송준혁 <fprhqkrtk303@naver.com>
2024-11-14 15:54:40 +0900

    feat: 운전자 차량 조회 API

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/service/VehicleReadService.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/vehicle/service/VehicleReadService.java
@@ -3,9 +3,28 @@ package ssafy.modo.jamkkaebi.domain.vehicle.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ssafy.modo.jamkkaebi.common.security.util.SecurityUtil;
+import ssafy.modo.jamkkaebi.domain.vehicle.dto.response.VehicleInfo;
+import ssafy.modo.jamkkaebi.domain.vehicle.entity.Vehicle;
+import ssafy.modo.jamkkaebi.domain.vehicle.exception.VehicleNotFoundException;
+import ssafy.modo.jamkkaebi.domain.vehicle.repository.VehicleRepository;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class VehicleReadService {
+
+    private final VehicleRepository vehicleRepository;
+    private final SecurityUtil securityUtil;
+
+    public VehicleInfo getVehicleInfo() {
+
+        Vehicle vehicle = vehicleRepository.findByDriverId(securityUtil.getCurrentUserId())
+                .orElseThrow(VehicleNotFoundException::new);
+
+        return VehicleInfo.builder()
+                .vehicleId(vehicle.getId())
+                .vehicleNumber(vehicle.getVehicleNumber())
+                .build();
+    }
 }
