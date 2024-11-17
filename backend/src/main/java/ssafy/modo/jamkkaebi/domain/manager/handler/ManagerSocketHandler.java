661444cc8c0e8a438ef179fd774e56c3bc47cddf
송준혁 <fprhqkrtk303@naver.com>
2024-11-17 11:16:44 +0900

    feat: 매니저 웹소켓 세션 관리
    - 매니저 접속 시 매니저에 연결관 운전자 목록 조회
    - 운전자에게 연결된 차량의 기기 목록 조회 후 상태 확인
    - 상태 확인된 기기 목록을 매니저 세션에 연결하여 HashMap에 저장

--- backend/src/main/java/ssafy/modo/jamkkaebi/domain/manager/handler/ManagerSocketHandler.java
+++ backend/src/main/java/ssafy/modo/jamkkaebi/domain/manager/handler/ManagerSocketHandler.java
@@ -9,8 +9,13 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import ssafy.modo.jamkkaebi.common.websocket.handler.SocketMessageHandler;
+import ssafy.modo.jamkkaebi.common.websocket.handler.SocketRequestType;
+import ssafy.modo.jamkkaebi.domain.device.entity.Device;
+import ssafy.modo.jamkkaebi.domain.device.service.DeviceReadService;
+import ssafy.modo.jamkkaebi.domain.manager.service.ManagerReadService;
+import ssafy.modo.jamkkaebi.domain.member.entity.Member;
 
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Component
@@ -18,6 +23,10 @@ import java.util.Map;
 public class ManagerSocketHandler extends TextWebSocketHandler {
 
     private final SocketMessageHandler socketMessageHandler;
+    private final ManagerReadService managerReadService;
+    private final DeviceReadService deviceReadService;
+
+    private final Map<WebSocketSession, Set<String>> managerDeviceMap = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
@@ -31,5 +40,22 @@ public class ManagerSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws JsonProcessingException {
 
         JSONObject jsonMessage = socketMessageHandler.buildJsonMessage(message);
+        String messageType = socketMessageHandler.getMessageType(jsonMessage);
+
+        if (messageType.equals(SocketRequestType.GET.toString())) {
+            Long managerId = ((Number) jsonMessage.get("managerId")).longValue();
+            List<Member> managedDrivers = managerReadService.getManagedDrivers(managerId);
+            Set<String> deviceSet = new HashSet<>();
+
+            managedDrivers.forEach(driver -> {
+                Device device = deviceReadService.getDeviceByDriverId(driver.getId());
+                if (device != null && deviceReadService.isDeviceHealthy(device)) {
+                    deviceSet.add(device.getUuid());
+                }
+            });
+
+            managerDeviceMap.put(session, deviceSet);
+            log.info("Device list: {}", deviceSet);
+        }
     }
 }
